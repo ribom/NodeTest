63fb0427a90970eec36e9d28df8c4e0ff8a8bbb5
ribom <gustav.ribom@gmail.com>
2016-08-16 22:21:25 +0200

    pictures to front page

--- front/searchMov/movieSearchController.js
+++ front/searchMov/movieSearchController.js
@@ -1,20 +1,18 @@
+var apiKey = 'dc7e3aace683c4dcbc23548159ec3cb3';
+var discoverUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=';
+var imgUrl = 'https://image.tmdb.org/t/p/w1280/';
 var app = angular.module('search', []);
-app.controller('searchCtrl', function ($scope, $http) {
+app.controller('mostPopCtrl', function ($scope, $http) {
     $scope.movieArray = [];
-
-    var apiKey = 'dc7e3aace683c4dcbc23548159ec3cb3';
-    var discoverUrl = 'https://api.themoviedb.org/3/discover/movie?api_key=';
-    var imgUrl = 'https://image.tmdb.org/t/p/w300_and_h450_bestv2/';
-
     $scope.baseImgUrl = imgUrl;
-
-    $scope.search = function () {
+    console.log("yes");
+    var getMovies = function () {
         $http({
-            method: 'GET',
-            url: discoverUrl + apiKey
+            method: 'GET'
+            , url: discoverUrl + apiKey
         }).
         success(function (data, status, headers, config) {
-            $scope.movieArray = data.results;
+            $scope.movieArray = data.results.slice(0, 5);
             console.log($scope.movieArray);
         }).
         error(function (data, status, headers, config) {
@@ -22,4 +20,5 @@ app.controller('searchCtrl', function ($scope, $http) {
         });
         $scope.word = $scope.term;
     };
+    getMovies();
 });
